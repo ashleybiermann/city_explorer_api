4ccefe7b1568d02db0061b36f324691f5bf42457
ashleybiermann <ashleybiermann@gmail.com>
2020-05-13 13:57:24 -0700

    added schema file with demo data and added some basic into to server js

--- server.js
+++ server.js
@@ -4,12 +4,18 @@
 require('dotenv').config(); // good to keep this one first, to avoid
 const express = require('express');
 const cors = require('cors');
+const pg = require('pg');
 const superagent = require('superagent');
 const PORT = process.env.PORT || 3001;
 const app = express(); //app is our entire server
 
 //configs
 app.use(cors()); // configure the app to talk to other local websites without blocking them
+
+// database config
+const client = new pg.Client(process.env.DATABASE_URL);
+client.on('error', console.error); // like console.log, but different color
+client.connect();
 
 //TODO: Format dates and times and days to match the cards
 function Location (entireDataObject, city) {
